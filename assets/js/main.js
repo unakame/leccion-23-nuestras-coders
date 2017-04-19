070acb774ac51f13fde1ab9f55dcfe2ba840c3f8
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-18 21:38:56 -0500

    setting images in HTML

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var marina = document.createElement("img");
+             marina.setAttribute("id","marina");
+             document.body.appendChild(marina);
+
+var lucy   = document.createElement("img");
+             lucy.setAttribute("id","lucy");
+             document.body.appendChild(lucy);
+
+var anai   = document.createElement("img");
+             anai.setAttribute("id","anai");
+             document.body.appendChild(anai);
+
+var mercurio = document.createElement("img");
+             mercurio.setAttribute("id","mercurio");
+             document.body.appendChild(mercurio);
+
+var neptuno = document.createElement("img");
+             neptuno.setAttribute("id","neptuno");
+             document.body.appendChild(neptuno);
+
+var urano   = document.createElement("img");
+             urano.setAttribute("id","urano");
+             document.body.appendChild(urano);
